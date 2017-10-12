796d5a7fe9baf9ced5008078e2b0f8bbc513c8d3
Simon Vandenbulcke <svandenbulcke@dxo.com>
2017-10-12 16:48:06 +0200

    add fonction to calculat size of file, and update key data on dictionnary

--- lib/list_directory.py
+++ lib/list_directory.py
@@ -1,15 +1,26 @@
 import os
 
-
 path = "/cygdrive/c/Users/svandenbulcke/Code/Perso/range_my_photos/tests"
+
+def convert_bytes(num):
+    """
+    this function will convert bytes to MB.... GB... etc
+    """
+    for x in ['bytes', 'KB', 'MB', 'GB', 'TB']:
+        if num < 1024.0:
+            return "%3.1f %s" % (num, x)
+        num /= 1024.0
+
 
 def listFiles(path):
     dictListFiles = {}
 
     for root, dirs, files in os.walk(path, topdown=False):
         for name in files:
-            print name
-            dictListFiles[name] = os.path.join(root, name)
+            pathOfFile = os.path.join(root, name)
+            dictListFiles[name] = {'extension'  : name[-3:],
+                                   'path'       : pathOfFile,
+                                   'size'       : convert_bytes(os.path.getsize(pathOfFile))}
 
     return dictListFiles
 
@@ -17,4 +28,9 @@ def listFiles(path):
 
 
 mlist_dir = listFiles(path)
-print('the list directory is {}'.format(mlist_dir))
+
+listkey1 = mlist_dir.keys()
+print('keys are {}'.format(listkey1))
+print ('value of runtest.txt {}'.format((mlist_dir[listkey1[1]].get('path'))))
+
+
