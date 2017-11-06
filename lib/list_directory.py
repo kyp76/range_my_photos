42cc049d3b199ac34586079197b2eb981e3ff868
Simon Vandenbulcke <svandenbulcke@dxo.com>
2017-11-06 13:54:11 +0100

    add TimeInformation about Exif and implemtent it on dico

--- lib/list_directory.py
+++ lib/list_directory.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import re
 
 # For EXIF reading
 head = os.path.dirname(os.path.realpath(__file__))
@@ -7,7 +8,8 @@ sys.path.append(os.path.join(head, 'exifread'))
 import exifread
 
 
-path = "/Users/svandenbulcke/Code/perso/range_my_photos/tests"
+#path = "/Users/svandenbulcke/Code/perso/range_my_photos/tests"
+path = '/Users/svandenbulcke/Photo_need_to_rank'
 
 
 def get_exif(image_filename, verbosity=None):
@@ -38,23 +40,51 @@ def listFiles(path):
     for root, dirs, files in os.walk(path, topdown=False):
         for name in files:
             pathOfFile = os.path.join(root, name)
+            try:
+                TimeOriginal = get_exif(pathOfFile)['EXIF DateTimeOriginal']
+            except KeyError:
+                TimeOriginal = 'Null'
+                pass
             dictListFiles[name] = {'extension'  : name[-3:],
                                    'path'       : pathOfFile,
-                                   'size'       : convert_bytes(os.path.getsize(pathOfFile))}
+                                   'size'       : convert_bytes(os.path.getsize(pathOfFile)),
+                                   'TimeOfPhoto': TimeOriginal
+                                   }
 
     return dictListFiles
 
+
+def extractTime(TimeOriginal):
+    TimeOriginalreformat = re.match('([0-9]{4}):([0-9]{2}):([0-9]{2}) ([0-9]{2}:[0-9]{2}:[0-9]{2})', TimeOriginal)
+    return TimeOriginalreformat.groups()
 
 
 
 mlist_dir = listFiles(path)
 
 listkey1 = mlist_dir.keys()
-print('mlist_dir is  {}'.format(listkey1[0]))
-pathImg = mlist_dir[listkey1[5]].get('path')
+print ('list of files present in my dico {}'.format(listkey1))
 
-#
-#print('keys are {}'.format(listkey1))
+
+#pathImg = mlist_dir[listkey1[5]].get('path')
+
+#TimeOriginal = get_exif(pathImg)['EXIF DateTimeOriginal']
+
+
+##TimeOriginalreformat = re.match('([0-9]{4}):([0-9]{2}):([0-9]{2}) ([0-9]{2}:[0-9]{2}:[0-9]{2})',str(TimeOriginal))
+
+###yearOfPhoto, monthOfPhoto, dayOfPhoto, hourOfPhoto = extractTime(str(TimeOriginal))
+
+
+print('full informations of dico are {}'.format(mlist_dir))
 #print ('path of {} is {}'.format(listkey1[0],(mlist_dir[listkey1[0]].get('path'))))
 
-print ('exif of {} is {}'.format(listkey1[5],get_exif(pathImg)))
+#print ('exif of {} is {}'.format(listkey1[5],dictExifValue))
+
+
+#TODO : methode to share information of dico folder , ext or folder , time .....
+
+#TODO : creation of directory depending of TimeOriginal
+
+#TODO argpass
+
